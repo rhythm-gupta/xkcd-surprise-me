6867a34ce7e2d7ccae83bee0531efe23a3978b60
Rhythm Gupta <rhythm.gupta21@gmail.com>
2013-01-19 00:04:46 +0530

    Update downall.py

--- downall.py
+++ downall.py
@@ -12,4 +12,6 @@ for i in range(1,1163):#corrently there are 1162 images on xkcd. this number is 
 	f=open(os.getcwd()+"\xkcd\"str(i)+'.png','wb')
 	f.write(urllib2.urlopen(soup.find_all(id="comic")[0].img['src']).read())
 	time.sleep(2)#I've used a timesleep of 2 seconds so that xkcd servers don't ban our ip.
+	#PS: You can comment this sleep function if you want to , cuz I've downloaded their whole directory but
+	#still they didn't ban my IP. ;)
 	print("Downloaded image no: "+str(i))
