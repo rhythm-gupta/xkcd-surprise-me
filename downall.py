844256249fcd3b0e5fe18c4b7b75657f1dea7aa3
monty-singh <monty.sinngh@gmail.com>
2013-07-14 19:10:39 +0530

    Few changes: made it a little more pythonic

--- downall.py
+++ downall.py
@@ -1,20 +1,25 @@
-***
+"""
 
 This file downloads all the images from xkcd.
 
-***
+"""
 
-import urllib2,time,os
+import urllib2, time, os
 from bs4 import BeautifulSoup
-for i in filter(lambda x:x!=404,range(1,1163)):#corrently there are 1162 images on xkcd. this number is subjected to change.
-    rand=urllib2.urlopen("http://xkcd.com/"+str(i))
-    soup=BeautifulSoup(rand.read())
-    di=os.getcwd()+"/xkcd/"
-    if not os.path.exists(di):
-        os.makedirs(di)
-    f=open(di+str(i)+'.png','wb')
-    f.write(urllib2.urlopen(soup.find_all(id="comic")[0].img['src']).read())
+#currently there are 1162 images on xkcd. this number is subjected to change.
+image_count = 1162
+url = "http://xkcd.com/"
+ext = '.png'
+for i in range(1,404) + range(405, image_count+1):
+    i = str(i)
+    data =  urllib2.urlopen(url + i)
+    soup = BeautifulSoup(data.read())
+    dir_name = os.path.join(os.getcwd(), "xkcd")
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+    with open(os.path.join(dir_name,(i + ext)), 'wb') as f2:
+        f2.write(urllib2.urlopen(soup.find_all(id="comic")[0].img['src']).read())
     time.sleep(2)#I've used a timesleep of 2 seconds so that xkcd servers don't ban our ip.
     #PS: You can comment this sleep function if you want to , cuz I've downloaded their whole directory but
     #still they didn't ban my IP. ;)
-    print("Downloaded image no: "+str(i))
+    print("Downloaded image no: "+ i)
